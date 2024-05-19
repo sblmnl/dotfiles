fec7f3beb5c099a77c4dbb0b8cea4daf07cb0593
sblmnl <sblmnl@users.noreply.github.com>
2024-05-19 13:12:49 -0400

    add flatpak install

--- scripts/install/apps/packages.sh
+++ scripts/install/apps/packages.sh
@@ -33,3 +33,7 @@ apt install -y \
     dstat \
     cmus \
     calcurse
+
+# install flatpak
+apt install -y flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
