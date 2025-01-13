d657aa2a20998b24b0f9035a6199c4f02c09e1d9
sblmnl <sblmnl@users.noreply.github.com>
2025-01-12 21:39:18 -0500

    add home drive to storage setup script

--- scripts/install/core/storage.sh
+++ scripts/install/core/storage.sh
@@ -13,21 +13,37 @@ swapon -va
 
 lvextend -r -l +100%FREE /dev/$VG/home
 
-# configure auto-mount for encrypted drive
-DISK_ID="88a6b901-6969-4f4f-a3c2-e91ec860d86b"
-PARTITION_ID="26529d89-8020-4fe5-84f1-c4a137e8786e"
-MOUNT_POINT="/media/Data"
-
-echo "luks-$DISK_ID UUID=$DISK_ID /etc/luks-keys/luks-$DISK_ID nofail" >> /etc/crypttab
-echo "/dev/disk/by-uuid/$PARTITION_ID $MOUNT_POINT auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
-
-read -p "Enter passphrase for $MOUNT_POINT: " PASSWORD
+# configure auto-mount for encrypted drives
 
 if [[ ! -d /etc/luks-keys ]]; then
     mkdir /etc/luks-keys
     chmod /etc/luks-keys 700
 fi
 
-echo -n "$PASSWORD" > /etc/luks-keys/luks-$DISK_ID
+# /media/Home
+DISK_ID_1="01c6ec40-8106-4503-ab2a-7dbfd53439b4"
+PARTITION_ID_1="b315385b-cdba-4471-9b82-dd532c38f228"
+MOUNT_POINT_1="/media/Home"
 
-unset DISK_ID PARTITION_ID MOUNT_POINT PASSWORD
+echo "luks-$DISK_ID_1 UUID=$DISK_ID_1 /etc/luks-keys/luks-$DISK_ID_1 nofail" >> /etc/crypttab
+echo "/dev/disk/by-uuid/$PARTITION_ID_1 $MOUNT_POINT_1 auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
+
+read -p "Enter passphrase for $MOUNT_POINT_1: " PASSWORD_1
+
+echo -n "$PASSWORD_1" > /etc/luks-keys/luks-$DISK_ID_1
+
+# /media/Data
+DISK_ID_2="88a6b901-6969-4f4f-a3c2-e91ec860d86b"
+PARTITION_ID_2="26529d89-8020-4fe5-84f1-c4a137e8786e"
+MOUNT_POINT_2="/media/Data"
+
+echo "luks-$DISK_ID_2 UUID=$DISK_ID_2 /etc/luks-keys/luks-$DISK_ID_2 nofail" >> /etc/crypttab
+echo "/dev/disk/by-uuid/$PARTITION_ID_2 $MOUNT_POINT_2 auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
+
+read -p "Enter passphrase for $MOUNT_POINT_2: " PASSWORD_2
+
+echo -n "$PASSWORD_2" > /etc/luks-keys/luks-$DISK_ID_2
+
+# cleanup
+unset DISK_ID_1 PARTITION_ID_1 MOUNT_POINT_1 PASSWORD_1 \
+    DISK_ID_2 PARTITION_ID_2 MOUNT_POINT_2 PASSWORD_2
